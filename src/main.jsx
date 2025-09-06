eaa936a49235880c60d1c88d7c75c4ef4733784d
Atul Sharma <techsharma04@gmail.com>
2025-09-06 01:33:03 -0400

    front page going on

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import App from './App.jsx'
 import { Provider } from 'react-redux'
 import { store } from './store/store.js'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap-icons/font/bootstrap-icons.css";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
